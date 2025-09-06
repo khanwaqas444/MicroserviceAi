d6b8fb0660fa21e9733cbfddad7ef3f659b499a1
khanwaqas444 <waqaskhan4447770@gmail.com>
2025-09-07 00:58:06 +0530

    MicroserviceAI

--- configserver/src/main/java/com/fitness/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/fitness/configserver/ConfigserverApplication.java
@@ -1,7 +1,6 @@
 package com.fitness.configserver;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
